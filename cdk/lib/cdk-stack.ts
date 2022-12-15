ad7d975e40839275140ce4f2227fb924b7f11d37
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-12-15 17:06:09 +0000

    feat: add example of Docker-based Lambda function

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as python from '@aws-cdk/aws-lambda-python-alpha';
-import { FunctionUrlAuthType, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Architecture, DockerImageCode, DockerImageFunction, FunctionUrlAuthType, Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import { CfnOutput } from 'aws-cdk-lib';
 
@@ -9,14 +9,24 @@ export class PythonLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const f = new python.PythonFunction(this, 'test-function', {
+    // Zip-based Lambda function.
+    const zipFunction = new python.PythonFunction(this, "zip-function", {
       entry: path.join(__dirname, '../../functions/test-function'),
       runtime: Runtime.PYTHON_3_8,
     })
-    const url = f.addFunctionUrl({ authType: FunctionUrlAuthType.NONE })
+    const zipUrl = zipFunction.addFunctionUrl({ authType: FunctionUrlAuthType.NONE })
+    new CfnOutput(this, "zip-function-url", {
+      value: zipUrl.url,
+    })
 
-    new CfnOutput(this, "function-url", {
-      value: url.url,
+    // Docker-based Lambda function.
+    const dockerFunction = new DockerImageFunction(this, "docker-function", {
+      architecture: Architecture.ARM_64,
+      code: DockerImageCode.fromImageAsset(path.join(__dirname, '../../functions/test-function')),
+    })
+    const dockerUrl = dockerFunction.addFunctionUrl({ authType: FunctionUrlAuthType.NONE })
+    new CfnOutput(this, "docker-function-url", {
+      value: dockerUrl.url,
     })
   }
 }
--- functions/test-function/index.py
+++ functions/test-function/index.py
@@ -1,6 +1,11 @@
 import json
+import boto3
+import numpy
 
 def handler(event, context):
+    s3 = boto3.resource('s3')
+    arr = numpy.array([1, 2, 3, 4, 5])
+    print(arr)
     print('request: {}'.format(json.dumps(event)))
     return {
         'statusCode': 200,
